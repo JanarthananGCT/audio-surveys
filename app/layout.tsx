6387f97110ffd411f98526eb0dd5bc7bdbf5b4f2
Janarthanan S <janarthanans@SSLAP0299.local>
2025-02-21 12:44:43 +0530

    answer updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Talk with Me 😍",
+  description: "Talk with Me 😍",
+  generator: "Heisenberg",
 }
 
 export default function RootLayout({
