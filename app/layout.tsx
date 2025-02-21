d243dfdd52be069dbc9be42471104d965e6d0b96
Janarthanan S <janarthanans@SSLAP0299.local>
2025-02-21 12:19:10 +0530

    Title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Talk with Me 😍',
+  description: 'Talk with Me 😍',
 }
 
 export default function RootLayout({
